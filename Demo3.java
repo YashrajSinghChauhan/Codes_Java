ebba0dfc99a74324bd6510c34050b06bd65d634f
harshitaYadav2003 <harshitaYadav2003>
2024-03-10 11:13:54 +0530

    demo3 Error program commited

---
+++ Demo3.java
@@ -0,0 +1,15 @@
+class Demo3
+{
+   static public void main(String args[])
+   {
+	    byte y= 10;
+	    System.out.println(x);
+	}
+
+}
+// byte
+// error: cannot find symbol
+        // System.out.println(x);
+  // symbol:   variable x
+  // location: class Demo3
+// 1 error
