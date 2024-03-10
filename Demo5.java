e4b5ef8d566539428321a8eb6d8a2cad3dd3914d
harshitaYadav2003 <harshitaYadav2003>
2024-03-10 11:33:51 +0530

    demo5 byte error program commited

---
+++ Demo5.java
@@ -0,0 +1,14 @@
+class Demo5
+{
+   static public void main(String args[])
+   {
+	    byte y= 130;
+        		
+	    System.out.println(y);
+   }
+
+}
+// byte - >    Range -128 - 127
+// Output ->  error: incompatible types: possible lossy 
+// conversion from int to byte
+            // byte y= 130;
