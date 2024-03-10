dfde8e7aadafc3279d27c804a50f376c760632bd
harshitaYadav2003 <harshitaYadav2003>
2024-03-10 11:36:28 +0530

    demo6 byte to int program commited

---
+++ Demo6.java
@@ -0,0 +1,11 @@
+class Demo6
+{
+   static public void main(String args[])
+   {
+	    byte y=123;
+        int x = y;	
+	    System.out.println("byte "+y);
+		System.out.println("int = "+x);
+   }
+
+}
